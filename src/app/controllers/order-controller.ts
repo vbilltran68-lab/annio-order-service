15accfaa776a397e8a4cf647fc05884219671c4f
Bill <tvkhue96@gmail.com>
2021-11-29 23:41:11 +0700

    update(controller): update response type

--- src/app/controllers/order-controller.ts
+++ src/app/controllers/order-controller.ts
@@ -1,11 +1,11 @@
-import { Controller, HttpStatus } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { ApiTags } from '@nestjs/swagger';
 import { BaseController } from '@annio/core/lib/controllers';
-import { ResponseDto } from '@annio/core/lib/dto';
 import { ORDER_ROUTES } from '@app/constants';
 import { OrderService } from '@app/services';
 import { CreateOrderDTO, OrderDTO } from '@app/dto';
+import { ORDER_STATUS } from '@annio/core/lib/interfaces';
 
 @ApiTags(ORDER_ROUTES.TAGS)
 @Controller()
@@ -15,28 +15,18 @@ export class OrderController extends BaseController {
   }
 
   @MessagePattern('order_create')
-  async create(body: CreateOrderDTO): Promise<ResponseDto<any>> {
-    return this.ApiResponse(HttpStatus.OK, 'Create Order Success', async () => {
-      const newOrder = await this.adminService.create(body);
-      return new OrderDTO(newOrder);
-    });
+  async create(body: CreateOrderDTO): Promise<OrderDTO> {
+    const newOrder = await this.adminService.create(body);
+    return new OrderDTO(newOrder);
   }
 
   @MessagePattern('order_cancel')
-  async cancel(id: string): Promise<ResponseDto<any>> {
-    return this.ApiResponse(
-      HttpStatus.OK,
-      'Cancel Order Success',
-      async () => await this.adminService.cancel(id),
-    );
+  async cancel(id: string): Promise<boolean> {
+    return await this.adminService.cancel(id);
   }
 
   @MessagePattern('order_cancel')
-  async checkOrderStatus(id: string): Promise<ResponseDto<any>> {
-    return this.ApiResponse(
-      HttpStatus.OK,
-      'Cancel Order Success',
-      async () => await this.adminService.checkStatus(id),
-    );
+  async checkOrderStatus(id: string): Promise<ORDER_STATUS> {
+    return await this.adminService.checkStatus(id);
   }
 }
