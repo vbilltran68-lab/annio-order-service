a267cfbe01a233503a25b049d9955ca9e05bef56
Bill <tvkhue96@gmail.com>
2021-12-03 23:10:54 +0700

    update(migration): add order table

--- src/app/migrations/1638547762098-create-order-table.ts
+++ src/app/migrations/1638547762098-create-order-table.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class createOrderTable1587053699221 implements MigrationInterface {
+export class createOrderTable1638547762098 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
     await queryRunner.createTable(this.table, true);
   }
